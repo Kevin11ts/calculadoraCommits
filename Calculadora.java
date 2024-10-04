dad4923c4582b68abcdc9f9408b9a597b4a29588
Kevin11ts <20233l001135@utcv.edu.mx>
2024-10-04 07:32:30 -0600

    feat: Añadir variables y mensajes para el usuario

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,21 @@
 import java.util.Scanner;
 
 public class Calculadora {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2;
+        char operacion;
+
+        System.out.println("Ingrese su primer número: ");
+        num1 = scanner.nextDouble();  
+
+        System.out.println("Ingrese el segundo número: ");
+        num2 = scanner.nextDouble();
+
+        System.out.println("Seleccione la operación (+, -, *, /): ");
+        operacion = scanner.next().charAt(0);
+
+       
+    }
 }
